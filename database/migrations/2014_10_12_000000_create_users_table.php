ff1fd7ae278a109f167b24a74850e72a31ae0a04
Cleberre22 <c.leberre@hotmail.fr>
2022-10-11 10:58:22 +0200

    maj

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('firstname', 100);
             $table->string('lastname', 100);
             $table->string('role', 100);
-            $table->string('email',)->unique();
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 60);
             $table->rememberToken();
